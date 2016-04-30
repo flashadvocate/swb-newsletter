f31e517b9bd2cfb76e2aafefb91aa050b2621ea1
flashadvocate <flashadvocate@gmail.com>
2016-04-30 10:47:26 -0400

    add more sections, gallery

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var screenshot_gallery = new Vue({
+    el: '#screenshot-gallery',
+    data: {
+        screenshots: [
+            {url: 'https://cdn1.vox-cdn.com/thumbor/vc_SqZ6ZRQlH2l020j2ROZ8ygSI=/cdn0.vox-cdn.com/uploads/chorus_asset/file/4160554/star_wars_battlefront_high-res_screenshot_berdu.0.jpg'},
+            {url: 'https://cdn0.vox-cdn.com/thumbor/cfA602HV5FhfIFraIWn1WRrMwes=/cdn0.vox-cdn.com/uploads/chorus_asset/file/4163506/star_wars_battlefront_x-wing_atst_high-res_screenshot_berdu.0.jpg'},
+            {url: 'https://cdn1.vox-cdn.com/thumbor/JlL2-InvMXJ4mVzxLSmApVCZg6M=/cdn0.vox-cdn.com/uploads/chorus_asset/file/4163520/star_wars_battlefront_tatooine_high-res_screenshot_berdu.0.jpg'},
+            {url: 'https://cdn2.vox-cdn.com/thumbor/XnKS_kLaO0sYigodgG7Px5svWQU=/cdn0.vox-cdn.com/uploads/chorus_asset/file/4163604/star_wars_battlefront_hoth_interior_high-res_screenshot_berdu.0.jpg'},
+        ]
+    }
+});
