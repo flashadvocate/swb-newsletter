e014d98ddd0e294ebb881bf42883d3f14a7959a5
gam3rsmurf <sahurst@gmail.com>
2016-05-18 00:03:16 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ var screenshot_gallery = new Vue({
             {url: 'https://cdn0.vox-cdn.com/thumbor/cfA602HV5FhfIFraIWn1WRrMwes=/cdn0.vox-cdn.com/uploads/chorus_asset/file/4163506/star_wars_battlefront_x-wing_atst_high-res_screenshot_berdu.0.jpg'},
             {url: 'https://cdn1.vox-cdn.com/thumbor/JlL2-InvMXJ4mVzxLSmApVCZg6M=/cdn0.vox-cdn.com/uploads/chorus_asset/file/4163520/star_wars_battlefront_tatooine_high-res_screenshot_berdu.0.jpg'},
             {url: 'https://cdn2.vox-cdn.com/thumbor/XnKS_kLaO0sYigodgG7Px5svWQU=/cdn0.vox-cdn.com/uploads/chorus_asset/file/4163604/star_wars_battlefront_hoth_interior_high-res_screenshot_berdu.0.jpg'},
+            {url: 'https://heavyeditorial.files.wordpress.com/2015/05/may-the-fourth-4th-be-with-you-memes-gifs-star-wars-day-18.gif?w=400"},
             {url: 'https://cdn2.vox-cdn.com/thumbor/XnKS_kLaO0sYigodgG7Px5svWQU=/cdn0.vox-cdn.com/uploads/chorus_asset/file/4163604/star_wars_battlefront_hoth_interior_high-res_screenshot_berdu.0.jpg'},
         ]
     }
