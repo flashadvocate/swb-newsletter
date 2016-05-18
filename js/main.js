e000ac545cc7d2add26bc7b3da416ea9baa51b54
gam3rsmurf <sahurst@gmail.com>
2016-05-18 00:56:41 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,12 +2,9 @@ var screenshot_gallery = new Vue({
     el: '#screenshot-gallery',
     data: {
         screenshots: [
-            {url: 'https://cdn1.vox-cdn.com/thumbor/vc_SqZ6ZRQlH2l020j2ROZ8ygSI=/cdn0.vox-cdn.com/uploads/chorus_asset/file/4160554/star_wars_battlefront_high-res_screenshot_berdu.0.jpg'},
-            {url: 'https://cdn0.vox-cdn.com/thumbor/cfA602HV5FhfIFraIWn1WRrMwes=/cdn0.vox-cdn.com/uploads/chorus_asset/file/4163506/star_wars_battlefront_x-wing_atst_high-res_screenshot_berdu.0.jpg'},
-            {url: 'https://cdn1.vox-cdn.com/thumbor/JlL2-InvMXJ4mVzxLSmApVCZg6M=/cdn0.vox-cdn.com/uploads/chorus_asset/file/4163520/star_wars_battlefront_tatooine_high-res_screenshot_berdu.0.jpg'},
-            {url: 'https://cdn2.vox-cdn.com/thumbor/XnKS_kLaO0sYigodgG7Px5svWQU=/cdn0.vox-cdn.com/uploads/chorus_asset/file/4163604/star_wars_battlefront_hoth_interior_high-res_screenshot_berdu.0.jpg'},
-            {url: 'https://heavyeditorial.files.wordpress.com/2015/05/may-the-fourth-4th-be-with-you-memes-gifs-star-wars-day-18.gif?w=400'},
-            {url: 'https://cdn2.vox-cdn.com/thumbor/XnKS_kLaO0sYigodgG7Px5svWQU=/cdn0.vox-cdn.com/uploads/chorus_asset/file/4163604/star_wars_battlefront_hoth_interior_high-res_screenshot_berdu.0.jpg'},
+            {url: 'http://imgur.com/bE9akdI.png'},
+            {url: 'https://photos-5.dropbox.com/t/2/AABLOzVL18DgymCeNARyZLVmWaIiFZGK3xd3Ybj0t72jdQ/12/276859481/png/32x32/3/1463565600/0/2/Screenshot%202016-04-23%2019.06.29.png/ELTHrogCGNIcIAEoAQ/L--cPKbagEXgJtkJJhbdZoo-XmDiDlQNFhfYDL9kE6o?size_mode=3&size=2048x1536'},
+            
         ]
     }
 });
