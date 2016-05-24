2b79e58786ac6d435235c58524154e87ef99a0e5
gam3rsmurf <sahurst@gmail.com>
2016-05-23 20:57:41 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var screenshot_gallery = new Vue({
     data: {
         screenshots: [
             {url: 'http://imgur.com/bE9akdI.png'},
-            {url: 'https://photos-5.dropbox.com/t/2/AABLOzVL18DgymCeNARyZLVmWaIiFZGK3xd3Ybj0t72jdQ/12/276859481/png/32x32/3/1463565600/0/2/Screenshot%202016-04-23%2019.06.29.png/ELTHrogCGNIcIAEoAQ/L--cPKbagEXgJtkJJhbdZoo-XmDiDlQNFhfYDL9kE6o?size_mode=3&size=2048x1536'},
+            {url: 'http://www.clanaod.net/forums/attachment.php?attachmentid=23940&d=1464055021'},
             {url: 'http://www.clanaod.net/forums/attachment.php?attachmentid=23790&d=1463268472'},
             {url: 'http://www.clanaod.net/forums/attachment.php?attachmentid=23794&d=1463268478'},
             
