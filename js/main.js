45aa0d00acf5d908b33591868063c30d236bebef
gam3rsmurf <sahurst@gmail.com>
2016-05-23 20:55:46 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var screenshot_gallery = new Vue({
         screenshots: [
             {url: 'http://imgur.com/bE9akdI.png'},
             {url: 'https://photos-5.dropbox.com/t/2/AABLOzVL18DgymCeNARyZLVmWaIiFZGK3xd3Ybj0t72jdQ/12/276859481/png/32x32/3/1463565600/0/2/Screenshot%202016-04-23%2019.06.29.png/ELTHrogCGNIcIAEoAQ/L--cPKbagEXgJtkJJhbdZoo-XmDiDlQNFhfYDL9kE6o?size_mode=3&size=2048x1536'},
-            {url: 'https://photos-4.dropbox.com/t/2/AAAlsezPQDNRa_CJAo9xkdGM4Q9Ykgk967kLjJ2PSltDog/12/276859481/png/32x32/3/1463569200/0/2/Screenshot%202016-04-24%2021.59.30.png/ELTHrogCGNIcIAEoAQ/uEuItUGWrD28vB_hjfd74Aaxr8wyt_hkVOhU6OO70DI?size_mode=3&size=2048x1536'},
+            {url: 'http://www.clanaod.net/forums/attachment.php?attachmentid=23790&d=1463268472},
             {url: 'http://www.clanaod.net/forums/attachment.php?attachmentid=23794&d=1463268478'},
             
         ]
